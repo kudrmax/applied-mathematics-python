41b14084736ec73ac266e9b584809943db286ba4
kudr.max <mdkudryashov@edu.hse.ru>
2024-02-20 17:00:24 +0300

    Задокументировал все функции в boids

--- boids/funcs.py
+++ boids/funcs.py
@@ -5,18 +5,32 @@ import numpy as np
 
 
 def init_boids(boids: np.ndarray, asp: float, vrange: tuple = (0., 1.)):
+    """
+    Функция, отвечающая за создание птиц
+
+    Parameters
+    ----------
+    boids
+    asp
+    vrange
+    """
     n = boids.shape[0]
     rng = np.random.default_rng()
-    boids[:, 0] = rng.uniform(0., asp, size=n)
-    boids[:, 1] = rng.uniform(0., 1., size=n)
-    alpha = rng.uniform(0., 2 * np.pi, size=n)
-    v = rng.uniform(*vrange, size=n)
-    boids[:, 2] = v * np.cos(alpha)
-    boids[:, 3] = v * np.sin(alpha)
+
+    # задаем рандомные начальные положения
+    boids[:, 0] = rng.uniform(0., asp, size=n)  # координата x
+    boids[:, 1] = rng.uniform(0., 1., size=n)  # координата y
+
+    # задаем рандомные начальные скорости
+    alpha = rng.uniform(0., 2 * np.pi, size=n)  # угол наклона вектора
+    v = rng.uniform(*vrange, size=n)  # рандомный модуль скорости
+    boids[:, 2] = v * np.cos(alpha)  # координата x
+    boids[:, 3] = v * np.sin(alpha)  # координата y
 
 
 def directions(boids: np.ndarray, dt: float):
     """
+    Функция для отрисовки векторов стрелок, т.е. для отрисовки в каком направлении движутся птицы
 
     Parameters
     ----------
@@ -25,33 +39,63 @@ def directions(boids: np.ndarray, dt: float):
 
     Returns
     -------
-    array of N x (x0, y0, x1, y1) for array painting
+    Массив вида N x (x0, y0, x, y) для отрисовки векторов стрелок
     """
-    return np.hstack((
-        boids[:, :2] - dt * boids[:, 2:4],
-        boids[:, :2]
-    ))
+    pos = boids[:, :2]  # положение в момент времени t
+    delta_pos = dt * boids[:, 2:4]  # как изменилось положение за dt
+    pos0 = pos - delta_pos  # положение в момент времени t-dt
+    return np.hstack((pos0, pos))
 
 
 def vclip(v: np.ndarray, vrange: tuple[float, float]):
-    norm = np.linalg.norm(v, axis=1)
-    mask = norm > vrange[1]
+    """
+    Если скорость выходит за разрешенные скорости, то мы обрезаем скорость
+
+    Parameters
+    ----------
+    v
+    vrange
+    """
+    norm = np.linalg.norm(v, axis=1)  # модуль скорости
+    mask = norm > vrange[1]  # маска
+    # @todo разобраться что делает эта штука (кажется обрезает вектор просто до значения модуля скорости vrange[1])
     if np.any(mask):
         # np.newaxis # np.reshape # norm[mask].reshape(-1, -1) -1 — автоматически посчитать
         v[mask] *= vrange[1] / norm[mask, None]
 
 
 def propagate(boids: np.ndarray, dt: float, vrange: tuple[float, float]):
+    """
+    Пересчет скоростей за время dt
+
+    Parameters
+    ----------
+    boids
+    dt
+    vrange
+    """
+    # @todo разобраться
     boids[:, 2:4] += 0.5 * dt * boids[:, 4:6]  # скорости # v = v*dt # возможно тут нужно убрать умножение на 0.5
     vclip(boids[:, 2:4], vrange)
     boids[:, 0:2] += dt * boids[:, 2:4]  # координаты # s = s_0 + v_0*dt + 0.5*a*dtˆ2
 
 
 def distances(vecs: np.ndarray) -> np.ndarray:
+    """
+    Вычисляет матрицу, где в ячейке (i, j) записано расстояние между птицей i и птицей j
+
+    Parameters
+    ----------
+    vecs
+
+    Returns
+    -------
+    Матрица c расстояниями
+    """
     n, m = vecs.shape
-    delta = vecs.reshape((n, 1, m)) - vecs.reshape((1, n, m))
-    D = np.linalg.norm(delta, axis=2)
-    return D
+    vector_distance_difference = vecs.reshape((n, 1, m)) - vecs.reshape((1, n, m))
+    norm_of_distance_difference = np.linalg.norm(vector_distance_difference, axis=2)
+    return norm_of_distance_difference
 
 
 def flocking(boids: np.ndarray,
@@ -59,18 +103,38 @@ def flocking(boids: np.ndarray,
              coeff: np.array,
              asp: float,
              vrange: tuple):
-    D = distances(boids[:, 0:2])
+    """
+    Функция, отвечающая за взаимодействие птиц между собой
+
+    Parameters
+    ----------
+    boids
+    perception
+    coeff
+    asp
+    vrange
+    """
+    D = distances(boids[:, 0:2])  # матрица с расстояниями между всеми птицами
     N = boids.shape[0]
-    D[range(N), range(N)] = perception + 1  # выкидываем из D растояние между i и i
-    mask = D < perception
-    wal = walls(boids, asp)
+    D[range(N), range(N)] = np.inf  # выкидываем расстояния между i и i
+    mask = D < perception  # если расстояние достаточно близкое (в круге радиуса perception), то True
+    # walls = create_walls(boids, asp)  # создание стен
     for i in range(N):
-        if not np.any(mask[i]):  # нет соседей
-            coh = np.zeros(2)
-            alg = np.zeros(2)
-            sep = np.zeros(2)
-        else:
-            coh = cohesion(boids, i, mask[i], perception)
-            coh = alignment(boids, i, mask[i], vrange)
-            coh = separation(boids, i, mask[i], perception)
-        boids[i, 4:6] = coeff[0] * coh + coeff[1] * alg + coeff[2] * sep + coeff[3] * wal[i]
+
+        # вычисляем насколько должны поменяться ускорения
+        # if not np.any(mask[i]):  # если нет соседей, то ничего не менять, то есть птица как двигалась, так и движется
+        #     separation = np.zeros(2)
+        #     alignment = np.zeros(2)
+        #     cohesion = np.zeros(2)
+        # else:
+        #     separation = compute_separation(boids, i, mask[i], perception)
+        #     alignment = compute_alignment(boids, i, mask[i], vrange)
+        #     cohesion = compute_cohesion(boids, i, mask[i], perception)
+
+        # @todo временно:
+        separation = np.zeros(2)
+        alignment = np.zeros(2)
+        cohesion = np.zeros(2)
+
+        # меняем ускорения птиц
+        boids[i, 4:6] = coeff[0] * cohesion + coeff[1] * alignment + coeff[2] * separation  # + coeff[3] * walls[i]
--- boids/main.py
+++ boids/main.py
@@ -9,20 +9,20 @@ from funcs import *
 app.use_app('pyqt5')
 # app.use_app('pyglet')
 
-w, h = 640, 480
-N = 500
-dt = 0.001
-asp = w / h
-perseption = 1 / 20
-vrange = (0, 0.1)
+w, h = 640, 480  # размеры экрана
+N = 500  # кол-во птиц
+dt = 0.01
+asp = w / h  # мультиплаер
+perseption = 1 / 20  #
+vrange = (0, 0.1)  # ограничения на скорости
 
-coeffs = np.array([0.05, 0.02, 4, 0.03])
+coeffs = np.array([0.05, 0.02, 4, 0.03])  # коэффициенты взаисодейлствя
 
-boids = np.zeros((N, 6), dtype=np.float64)  # x, y, vx, vy, ax, ay
-init_boids(boids, asp, vrange=vrange) # тут ускорения
-boids[:, 4:6] = 0.1
+boids = np.zeros((N, 6), dtype=np.float64)  # одна строка матрица <-> одна птица с параметрами [x, y, vx, vy, ax, ay]
+init_boids(boids, asp, vrange=vrange)  # создаем птиц
+boids[:, 4:6] = 0.1  # задаем птицам ускорения
 
-canvas = scene.SceneCanvas(show=True, size=(w, h))
+canvas = scene.SceneCanvas(show=True, size=(w, h))  # создаем сцену
 view = canvas.central_widget.add_view()
 view.camera = scene.PanZoomCamera(rect=Rect(0, 0, asp, 1))
 arrows = scene.Arrow(arrows=directions(boids, dt),
@@ -32,14 +32,18 @@ arrows = scene.Arrow(arrows=directions(boids, dt),
                      parent=view.scene)
 
 
+def paint_arrows(arrows):
+    arrows.set_data(arrows=directions(boids, dt))  # отрисовка стрелок
+
+
 def update(event):
-    flocking(boids, perseption, coeffs, asp, vrange)
-    propagate(boids, dt, vrange)
-    arrows.set_data(arrows=directions(boids, dt))
-    canvas.update()
+    flocking(boids, perseption, coeffs, asp, vrange)  # пересчет ускорений (взаимодействие между птицами)
+    propagate(boids, dt, vrange)  # пересчет скоростей на основе ускорений
+    paint_arrows(arrows)  # отрисовка стрелок
+    canvas.update()  # отображение
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # @todo это
     timer = app.Timer(interval=0, start=True, connect=update)
     canvas.measure_fps()
     app.run()
