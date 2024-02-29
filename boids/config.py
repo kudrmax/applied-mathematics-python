398f2ab767b4d7f070a0204608b630192a3e7e5f
kudr.max <mdkudryashov@edu.hse.ru>
2024-02-29 17:06:28 +0300

    Пофиксил cohision

--- boids/config.py
+++ boids/config.py
@@ -9,19 +9,19 @@ perception_radius = 1 / 30
 velocity_range = (0, .1)  # ограничения на скорости
 acceleration_range = (0, .1)  # ограничения на ускорения
 
-slider_multiplier = 1000
+slider_multiplier = 10000
 cohesion_range = (0, .5)
 separation_range = cohesion_range
 alignment_range = cohesion_range
-perception_radius_range = (1, 40)
+perception_radius_range = (1, 80)
 
 max_speed_magnitude = 4
 max_delta_velocity_magnitude = 10
 
 coeffs = {
-    'cohesion': .0,
-    'separation': .0,
-    'alignment': .0,
+    'cohesion': .001,
+    'separation': .001,
+    'alignment': .001,
 }
 
 
@@ -32,4 +32,4 @@ size = np.array([W / H, 1])
 cohesion_range = (int(cohesion_range[0] * slider_multiplier), int(cohesion_range[1] * slider_multiplier))
 separation_range = (int(separation_range[0] * slider_multiplier), int(separation_range[1] * slider_multiplier))
 alignment_range = (int(alignment_range[0] * slider_multiplier), int(alignment_range[1] * slider_multiplier))
-# perception_radius_range = (int(perception_radius_range[0] * slider_multiplier), int(perception_radius_range[1] * slider_multiplier))
+perception_radius_range = (int(perception_radius_range[0] * slider_multiplier), int(perception_radius_range[1] * slider_multiplier))
